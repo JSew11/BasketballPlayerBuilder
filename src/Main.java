587dc81f81edba499566866088270c6907e9d5b6
Joshua Seward <joshuaseward11@gmail.com>
2023-07-12 16:40:52 -0700

    removed default comments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import player.Player;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Player player = new Player();
